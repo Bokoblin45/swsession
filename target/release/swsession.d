5b9db9f2af6219f63df33ee2361929b64221b4dc
Bokoblin45 <Lewineijzen@proton.me>
2026-09-24 00:07:04 +0200

    generated my binary

--- target/release/swsession.d
+++ target/release/swsession.d
@@ -1,1 +1,1 @@
-/home/deck/Documents/GitHub/swsession/target/release/swsession: /home/deck/Documents/GitHub/swsession/src/main.rs
+/home/deck/Projects/swsession/target/release/swsession: /home/deck/Projects/swsession/src/main.rs
